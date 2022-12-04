14c0c17d992886485c5be53cfaf7f3e9185bed2b
BohdaR <bohdanshushval@gmail.com>
2022-12-04 19:50:59 +0200

    set up .env

--- LB_7/db.php
+++ LB_7/db.php
@@ -1,3 +1,10 @@
 <?php
-    $connection_string = "host=localhost port=5432 dbname=postgres user=postgres password=nafta#08752";
-    $CONNECTION = pg_connect($connection_string) or die("Could not connect");
+
+declare(strict_types=1);
+require_once('../vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable('../');
+$dotenv->load();
+
+$connection_string = "host=" . $_ENV['HOST'] . " port=5432 dbname=" . $_ENV['DB_NAME'] . " user=" . $_ENV['DB_USER'] . " password=" . $_ENV['DB_PASSWORD'];
+$CONNECTION = pg_connect($connection_string) or die("Could not connect");
